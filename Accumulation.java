f1f5ce1e8b7c14813a81e8ff7fffb45e35a1296e
KammariRahul <rahulkammari01@gmail.com>
2024-12-24 12:41:29 +0530

    first commit

---
+++ Accumulation.java
@@ -0,0 +1,31 @@
+package com.rahul;
+
+public class Accumulation {
+
+		public static void main(String[] args) {
+			
+			int num = 9999,r,sum=0;
+			// 9+9+9+9---36---3+6---9
+
+			while(num !=0) {
+				r = num % 10;// 
+				num = num /10;
+				sum = sum + r;
+				if(num == 0) {
+					if(sum >= 10) {
+						num = sum;
+						sum =0;
+					}
+				}
+			}
+			System.out.println("sum--->"+sum);
+		}
+
+	}
+	
+
+       
+    
+
+
+
