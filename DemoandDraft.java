d0701153948b0295975f94e9a4830a08f00be820
KammariRahul <rahulkammari01@gmail.com>
2024-12-24 15:44:20 +0530

    first commit

---
+++ DemoandDraft.java
@@ -0,0 +1,74 @@
+package com.rahul;
+
+public class DemoandDraft {
+	
+		public static void main(String[] args) {
+
+			// one two........nine
+			// eleven twelve thirteen............nineteen
+			// ten twenty thirty forty fifty...........ninety
+
+			int n = 7425, d;
+			
+			String dd = "";
+			int p=0,first=0;
+			while (n != 0) {
+				d = n % 10;
+				n = n / 10;
+				p++;
+				if(p==1) {
+					dd = dd + convertToWord(d);
+					first=d;
+				}
+				else if(p==2) {
+					if(d==1) {
+						dd = convertToOnes(first);
+					}
+					else{
+						dd =  convertToTens(d)+" "+dd;
+					}
+						
+				}
+				else if(p==3) {
+					dd =  convertToWord(d)+" hundred "+ dd ;
+				}
+				else if(p==4) {
+					dd =  convertToWord(d)+" thousand "+ dd ;
+				}
+				else if(p==5) {
+					dd =  convertToTens(d)+" "+ dd ;
+				}
+			}
+
+			System.out.println(dd);
+
+		}
+
+		private static String convertToOnes(int d) {
+			String[] data = {"","elevan", "twelve", "thirteen","fourteen","fifteen",
+					"sixteen","seventeen","eighteen","ninteen"};
+
+			return data[d];
+
+		}
+
+		private static String convertToTens(int d) {
+			String[] data = {"","ten","twenty","thirty","fourty","fifty",
+					"sixty","seventy","eighty","ninety"};
+
+			return data[d];
+		}
+
+		private static String convertToWord(int d) {
+			
+			String[] data = {"zero","one","two","three","four",
+					"five","six","seven","eight","nine"};
+
+			return data[d];
+
+					
+		}
+
+	}
+
+
